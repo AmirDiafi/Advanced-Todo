dba138885b9c56263bf410afd4993a4618157113
Amir Diafi <59441376+AmirDiafi@users.noreply.github.com>
2022-11-27 12:09:49 +0100

    feat: add empty message

---
+++ src/components/ViewContent/atoms/EmptyList.tsx
@@ -0,0 +1,12 @@
+export const EmptyList = () => {
+  return (
+    <div className='empty_list_container'>
+      <div className='empty_list'>
+        <h2 className='empty_list_title'>No todo yet❗️</h2>
+        <p className='empty_list_subtitle'>
+          Start your journey by adding your Todos 💪
+        </p>
+      </div>
+    </div>
+  )
+}
--- src/components/ViewContent/index.tsx
+++ src/components/ViewContent/index.tsx
@@ -1,16 +1,13 @@
 import './style/index.css'
 import { useTodoStore } from '../../store/todos'
 import { TodoList } from './components/TodoList'
+import { EmptyList } from './atoms/EmptyList'
 
 const ViewContent = () => {
   const { store } = useTodoStore()
   return (
     <div className='todo_content_container'>
-      {store.length === 0 ? (
-        'You have no todo yet 🙂'
-      ) : (
-        <TodoList data={store} />
-      )}
+      {store.length === 0 ? <EmptyList /> : <TodoList data={store} />}
     </div>
   )
 }
